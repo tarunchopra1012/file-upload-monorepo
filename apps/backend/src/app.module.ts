9825e6989e3beae58edd790967ba195f7eadc0f4
tarun.chopra <tarun.chopra@inapp.com>
2025-02-25 18:42:48 +0530

    Adding Graphql and Typeorm support in project

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UploadModule],
+  imports: [
+    UploadModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345678',
+      database: 'invoiceapp',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
